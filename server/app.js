f747ea8104b68ef4bf313e9423aa76bd5906227c
garychongqui <gary.chongqui91@gmail.com>
2020-11-18 02:45:06 -0500

    added user model,controller, and middleware/authentication. we can now create users and data can also be stored in mongoDB

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
+//We're importing db
 require('./db/config');
 const express = require('express'),
   path = require('path'),
-  openRoutes = require('./routes/open');
+  openRoutes = require('./routes/open'),
+  cookieParser = require('cookie-parser');
+passport = require('./middleware/authentication');
 
 const app = express();
 
-//Middleware
+//Middleware (parse incoming JSON into objects)
 app.use(express.json());
 
 // Unauthenticated routes
-app.use(openRoutes);
+app.use('/api/users', openRoutes);
+
+app.use(cookieParser());
 
 // Serve any static files
 if (process.env.NODE_ENV === 'production') {
