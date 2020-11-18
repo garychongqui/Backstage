4bf84d18ce2bc536a568e0e8112f4f4933214986
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2020-11-17 21:36:18 -0500

    firstLayout

--- server/app.js
+++ server/app.js
@@ -1,27 +1,38 @@
-require('./db/config');
+//import the DB connection
+require('./db/config/index');
 const express = require('express'),
-  path = require('path'),
-  openRoutes = require('./routes/open');
-
-const app = express();
-
-//Middleware
+  app = express(),
+  openRoutes = require('./routes/open/usersOpen'),
+  userRouter = require('./routes/secure/usersSecure'),
+  passport = require('./middleware/authentication/index'),
+  fileUpload = require('express-fileupload'),
+  cookieParser = require('cookie-parser'),
+  path = require('path');
+// Parse incoming JSON into objects
 app.use(express.json());
-
-// Unauthenticated routes
-app.use(openRoutes);
-
-// Serve any static files
+//Unauthenticated routes
+app.use('/api', openRoutes);
+//Middleware to parse through incoming cookies in the requests.
+app.use(cookieParser());
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
 }
-
-// Any authentication middleware and related routing would be here.
-
-// Handle React routing, return all requests to React app
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/images'
+  })
+);
+//Authenticated Routes
+app.use('/api/*', passport.authenticate('jwt', { session: false }));
+app.use('/api/users', userRouter);
 if (process.env.NODE_ENV === 'production') {
+  // Handle React routing, return all requests to React app
   app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    response.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
   });
 }
 module.exports = app;
