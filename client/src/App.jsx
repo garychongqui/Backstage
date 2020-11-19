c0840b99e076686a4e0c96a87037d27c4602c906
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2020-11-18 22:50:00 -0500

    brewing up the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 import './App.css';
 
