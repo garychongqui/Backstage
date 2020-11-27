75935e563dcfaea017f62ddf0098a4e6e954228c
Daviduf11 <ufberg.david@gmail.com>
2020-11-26 23:59:49 -0500

    updated App.jsx router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { AppContextProvider } from './context/AppContext';
 import Home from './pages/Home';
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './components/Login';
+// import Studio from './collaboration/Studio';
+import test from './components/test';
+import ArtistCollab from './pages/collaboration/ArtistCollab';
 
 function App() {
   return (
     <AppContextProvider>
-
-      <Home />
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/asrtist" component={ArtistCollab} />
+        </Switch>
+      </BrowserRouter>
     </AppContextProvider>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Login from '../components/Login';
-import Studio from './collaboration/Studio';
-import test from '../components/test';
-import ArtistCollab from './collaboration/ArtistCollab';
+import { Link, useHistory } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <h1>This is the Home Page</h1>
-      <BrowserRouter>
-        <Login />
-        <br />
-        <Link to="/dashboard">Click here for dashboard</Link>
-        <br />
-
-
-        <Link to="/artist">Click here for artist collaboration page</Link>
-
-
-        <br />
-        <br />
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-
-
-        <Route path="/artist">
-
-
-          <ArtistCollab />
-        </Route>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/studio" component={Studio} />
-          <Route exact path="/test" component={test} />
-        </Switch>
-
-        <Link className="mt-4" to="/studio">
-          Drag n Drop
-        </Link>
-      </BrowserRouter>
+      <h1>This is the Home Page (Landing Page)</h1>
+      <br />
+      <br />
+      <Link to="/login">Click here for login page</Link>
+      <br />
+      <Link to="/signup">Click here for signup page</Link>
     </div>
   );
 };
