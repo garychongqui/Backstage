b6b14fdecfbae7efa3e3e95513f98b683399905e
garychongqui <gary.chongqui91@gmail.com>
2020-11-28 18:02:52 -0500

    verified signup button with mongodb and add tailwind css to the signup component

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import swal from 'sweetalert';
 
 const SignUp = () => {
   const history = useHistory();
+  const [signupData, setSignUpData] = useState({});
+
+  const handleChange = (event) => {
+    setSignUpData({ ...signupData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/users/signup', signupData);
+    } catch (error) {
+      swal('SingUp unsuccessful. Please try again.', { icon: 'error' });
+    }
+
+    history.push('/dashboard/events');
+  };
   return (
-    <div className="singup-container">
+    <div className="signup-container">
       <div className="main-area">
         <form
           className="form"
           name="signup-form"
           method="POST"
           action="/api/users/signup"
+          onSubmit={handleSubmit}
         >
+          <div className="signup-title">
+            <h1 className="text-ob">Welcome Backstage..</h1>
+          </div>
           <div className="inner-form">
-            <h1 className="text-ob">Welcome Backstage</h1>
             <input
               className="text-input"
               type="text"
               placeholder="Username"
               name="name"
+              onChange={handleChange}
             />
           </div>
           <div className="inner-form">
@@ -27,6 +49,7 @@ const SignUp = () => {
               type="email"
               placeholder="Email"
               name="email"
+              onChange={handleChange}
             />
           </div>
           <div className="inner-form">
@@ -35,17 +58,11 @@ const SignUp = () => {
               name="password"
               className="text-input"
               placeholder="Password"
+              onChange={handleChange}
             />
           </div>
-          <div className="btn-area">
-            <input
-              type="submit"
-              className="btn-1"
-              value="SignUp"
-              onClick={() => {
-                history.push('/dashboard/events');
-              }}
-            />
+          <div className="signup-button-area">
+            <input type="submit" className="signup-button" value="SignUp" />
           </div>
         </form>
       </div>
