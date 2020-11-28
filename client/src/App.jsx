4589df6331fd7e716af38acc6bfe5087879cbf63
Daviduf11 <ufberg.david@gmail.com>
2020-11-28 11:47:06 -0500

    removed MainNav duplicate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,11 +15,9 @@ import SignUp from './components/SignUp';
 // import Studio from './collaboration/Studio';
 import test from './components/test';
 import ArtistCollab from './pages/collaboration/ArtistCollab';
-import MainNav from './components/mainNav/MainNav';
 import MyEvents from './components/events/MyEvents';
 import MyStages from './components/stages/MyStages';
 import MyEquipment from './components/equipment/MyEquipment';
-
 
 function App() {
   return (
