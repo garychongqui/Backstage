171de5d1a3f3163c9c73d17e167b93cbda7eccf0
Daviduf11 <ufberg.david@gmail.com>
2020-11-29 10:45:03 -0500

    pulled artist collab styling

--- client/src/pages/collaboration/Stage.jsx
+++ client/src/pages/collaboration/Stage.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 import equipLists from '../../artistEquip';
+import { jsPDF } from 'jspdf';
+import * as html2canvas from 'html2canvas';
+import autotable from 'jspdf-autotable';
 
 class Stage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  rows = this.props.eventData;
+
+  generatePdf = () => {
+    const newPlot = document.getElementById('theStage');
+    html2canvas(newPlot).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.text(`${this.props.eventData?.eventDate}`, 20, 20).setFontSize(32);
+      pdf.text(`${this.props.eventData?.eventTitle}`, 20, 45).setFontSize(16);
+      pdf.addImage(imgData, 'PNG', 15, 55, 160, 110);
+      pdf.text(
+        this.props.items?.map((item) => item),
+        20,
+        180
+      );
+      // pdf.text(['Item'].concat(this.props.items?.map((item) => item)), 20, 180);
+      pdf.text(
+        this.props.descriptions?.map((item) => item),
+        70,
+        180
+      );
+      // pdf.text(
+      //   this.props.quantities?.map((item) => item),
+      //   90,
+      //   180
+      // );
+      pdf.save(
+        `${this.props.eventData?.eventTitle} / ${this.props.eventData?.eventDate}`
+      );
+    });
+  };
   state = {
     activeDrags: 0,
     deltaPosition: {
@@ -10,52 +48,52 @@ class Stage extends React.Component {
       y: 0
     }
   };
-
   onStart = () => {
     this.setState({ activeDrags: ++this.state.activeDrags });
   };
-
   onStop = () => {
     this.setState({ activeDrags: --this.state.activeDrags });
   };
-
   render() {
     const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
     return (
-      <div
-        className="the-stage border border-gray-200 shadow-lg w-3/4 rounded-md relative flex flex-wrap items-start"
-        style={{
-          height: '32rem'
-        }}
-      >
-        {this.props.iconsForStage?.map((icon) => {
-          return (
-            <Draggable bounds="parent" {...dragHandlers}>
-              <div
-                className="icon-on-stage hover: cursor-pointer"
-                style={{
-                  backgroundImage: `url("${icon}")`,
-                  height: `5.5rem`,
-                  width: `5.5rem`,
-                  backgroundSize: '100% 100%'
-                }}
-              ></div>
-            </Draggable>
-          );
-        })}
-        {/* <div className="text-xs">
-        Icons made by{' '}
-        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div> */}
+      <div class="w-3/4 relative">
+        <div
+          className="the-stage w-full  rounded-md  flex flex-wrap items-start"
+          id="theStage"
+          style={{
+            height: '36.25rem',
+            backgroundColor: '#FFF7F1',
+            border: '4px solid #A6271F',
+            marginBottom: '0.25rem',
+            top: '0'
+          }}
+        >
+          {this.props.iconsForStage?.map((icon) => {
+            return (
+              <Draggable bounds="parent" {...dragHandlers}>
+                <div
+                  className="icon-on-stage hover: cursor-pointer"
+                  style={{
+                    backgroundImage: `url("${icon}")`,
+                    height: `7.5rem`,
+                    width: `7.5rem`,
+                    backgroundSize: '100% 100%'
+                  }}
+                ></div>
+              </Draggable>
+            );
+          })}
+        </div>
+        <button
+          className="btn-3"
+          onClick={this.generatePdf}
+          stlye={{ position: 'absolute', top: '40rem' }}
+        >
+          Download PDF
+        </button>
       </div>
     );
   }
 }
-
 export default Stage;
