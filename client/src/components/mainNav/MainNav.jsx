8e5a7af7e3d2e6273ec7d93e060a68f182c908b9
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2020-11-30 15:40:29 -0500

    fixing tailwind nav

--- client/src/components/mainNav/MainNav.jsx
+++ client/src/components/mainNav/MainNav.jsx
@@ -24,14 +24,6 @@ const MainNav = () => {
                 alt="dj icon"
                 style={{ height: '100%' }}
               />
-              {/* 
-              <svg
-                className="main-nav-svg"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{' '}
-              </svg> */}
             </button>
 
             <ul className="dropdown-menu absolute hidden text-white bg-gray-dark p-2 rounded">
