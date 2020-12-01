ac7a8f311712deca54a924f25905a50479fe510a
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2020-12-01 04:27:17 -0500

    making styles uniform tailwindcss

--- client/src/components/events/MyEvents.jsx
+++ client/src/components/events/MyEvents.jsx
@@ -24,31 +24,31 @@ const MyEvents = () => {
 
   return (
     <div
-      className="bg-dark-gray flex flex-col items-center"
-      style={{ marginTop: '4rem' }}
+      className="saved-event-area"
+      // style={{ marginTop: '4rem' }}
     >
       {events?.map((event1) => {
         return (
-          <div className="flex flex-col items-center w-full">
+          <div className="saved-event-card-area">
             <div
-              className="flex p-2 m-4 w-7/12 justify-center my-event-card"
-              style={{
-                borderTopRightRadius: '0.5rem',
-                borderBottomRightRadius: '0.5rem',
-                backgroundColor: '#FFF7F1',
-                marginBottom: '.4rem',
-                borderLeft: '8px solid #A6271F'
-              }}
+              className="saved-event-inner-card-area"
+              // style={{
+              //   borderTopRightRadius: '0.5rem',
+              //   borderBottomRightRadius: '0.5rem',
+              //   backgroundColor: '#FFF7F1',
+              //   marginBottom: '.4rem',
+              //   borderLeft: '8px solid #A6271F'
+              // }}
             >
-              <div className="p-2 m-2 flex flex-col items-center w-full h-56 mb-6 ml-36">
-                <h2 className="dash-h2 text-3xl">{event1?.eventTitle}</h2>
+              <div className="saved-event-inner-card-text-area">
+                <h2 className="dash-h2">{event1?.eventTitle}</h2>
                 <br />
-                <span className="text-2xl">
+                <span className="saved-event-card-custom-date">
                   {moment(event1?.eventDate).format('MMMM Do YYYY')}
                 </span>
                 <br />
-                <span
-                  className="text-2xl semibold"
+                {/* <span
+                  className="saved-event-has-been-opened-text"
                   style={
                     event1?.hasBeenOpened
                       ? { color: 'green', fontWeight: '600' }
@@ -56,10 +56,10 @@ const MyEvents = () => {
                   }
                 >
                   {event1?.hasBeenOpened ? 'Opened by Artist' : 'Not Opened'}
-                </span>
+                </span> */}
                 <br />
               </div>
-              <div className="text-center justify-center m-3 p-2 w-1/3"></div>
+              <div className="saved-event-card-last"></div>
             </div>
           </div>
         );
--- client/src/components/stages/NewStage.jsx
+++ client/src/components/stages/NewStage.jsx
@@ -56,15 +56,17 @@ const NewStage = () => {
               ></input>
             </div>
           </div>
-          <div className="new-stage-place mt-10">
+          <div className="new-stage-place">
             <label
-              for="outdoor-checkbox"
-              style={{ paddingTop: '8px' }}
-              className="text-xl"
+              className="outdoor-checkbox-label"
+              // for="outdoor-checkbox"
+              // style={{ paddingTop: '8px' }}
+              // className="text-xl"
             >
               Outdoor
             </label>
             <input
+              className="outdoor-checkbox-input"
               type="checkbox"
               name="isOutdoor"
               id="outdoor-checkbox"
@@ -75,9 +77,7 @@ const NewStage = () => {
           <br />
           <div className="comment-box">
             <div className="new-stage-comments-title">
-              <label className="dash-h3" for="comments-text-area">
-                Comments
-              </label>
+              <label className="dash-h3">Comments</label>
             </div>
             <div className="new-stage-comments">
               <textarea
